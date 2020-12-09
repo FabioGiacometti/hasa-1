0351861f4d40007e39e53045c69305a7285b8d3e
diegoeherrera <deherrera@gmail.com>
2020-12-09 10:25:48 -0300

    primer commit carousel

---
+++ app.js
@@ -0,0 +1,25 @@
+/* var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel,{
+    interval: 2000,
+    wrap: false,
+    pause:'hover',
+    cycle:true
+})
+
+carousel.cycle() */
+
+/* $('.carousel').carousel({
+    interval: 2000
+  }) */
+/*   $(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+});
+
+$('.carousel-control.left').click(function() {
+    $('#myCarousel').carousel('prev');
+  });
+  
+  $('.carousel-control.right').click(function() {
+    $('#myCarousel').carousel('next');
+  }); */
