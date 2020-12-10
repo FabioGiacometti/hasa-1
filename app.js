f8a70e0c7a2599e161b337a942bb9123f060c83b
diegoeherrera <deherrera@gmail.com>
2020-12-10 16:47:42 -0300

    slider funcando

--- app.js
+++ app.js
@@ -7,4 +7,29 @@ document.addEventListener( 'DOMContentLoaded', function () {
         fixedWidth:250
 
     } ).mount()
-} )
+    var  slides= document.querySelectorAll(".splide__slide");	
+    for (let slide of slides) {
+        slide.addEventListener("mouseenter", function(e){
+            var parent = e.target.firstElementChild;
+            console.log(parent)
+            if(parent){
+        
+                    parent.style.opacity="1"
+                }
+                document.getElementsByClassName("splide__arrows")[0].style.opacity="0";
+
+            })
+
+
+        slide.addEventListener("mouseleave", function(e){
+            var parent = e.target.firstElementChild;
+            console.log(parent)
+             if(parent){  
+                parent.style.opacity="0"
+            }
+            document.getElementsByClassName("splide__arrows")[0].style.opacity="1"; 
+        })    
+            
+    }
+          
+});
