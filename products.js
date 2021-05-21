a475d7c5d5a5cf6e835b318d85640f2e81f10d79
fabio.giacometti <fabiog.inbox@gmail.com>
2021-05-21 01:22:58 -0300

    new product images

--- products.js
+++ products.js
@@ -4,51 +4,51 @@ const products = [
       name: "Carretel reduccion - ramal Te - ramal Y - Curva 90º",
     },
     {
-      image: "./assets/imgs/productos/product_(2).png",
+      image: "./assets/imgs/productos/product_(2).jpg",
       name: "Conexion reduccion amplia tolerancia",
     },
     {
-      image: "./assets/imgs/productos/product_(3).png",
+      image: "./assets/imgs/productos/product_(3).jpg",
       name: "Curva manifold",
     },
     {
-      image: "./assets/imgs/productos/product_(4).png",
+      image: "./assets/imgs/productos/product_(4).jpg",
       name: "Junta conexión doble buloneria",
     },
     {
-      image: "./assets/imgs/productos/product_(5).png",
+      image: "./assets/imgs/productos/product_(5).jpg",
       name: "Junta conexion simple buloneria",
     },
     {
-      image: "./assets/imgs/productos/product_(6).png",
+      image: "./assets/imgs/productos/product_(6).jpg",
       name: "Junta de desmontaje autoportante",
     },
     {
-      image: "./assets/imgs/productos/product_(7).png",
+      image: "./assets/imgs/productos/product_(7).jpg",
       name: "Manifold con doble salida tangencial",
     },
     {
-      image: "./assets/imgs/productos/product_(8).png",
+      image: "./assets/imgs/productos/product_(8).jpg",
       name: "Ramal cruz reduccion con curvas",
     },
     {
-      image: "./assets/imgs/productos/product_(9).png",
+      image: "./assets/imgs/productos/product_(9).jpg",
       name: "Ramal tangencial con doble salida y curva al final",
     },    
     {
-    image: "./assets/imgs/productos/product_(10).png",
+    image: "./assets/imgs/productos/product_(10).jpg",
     name: "Ramal Y",
     },  
     {
-    image: "./assets/imgs/productos/product_(11).png",
+    image: "./assets/imgs/productos/product_(11).jpg",
     name: "Reducciones unidas por una valvula",
     },  
     {
-    image: "./assets/imgs/productos/product_(12).png",
+    image: "./assets/imgs/productos/product_(12).jpg",
     name: "Tambor de reparación",
     },  
     {
-    image: "./assets/imgs/productos/product_(13).png",
+    image: "./assets/imgs/productos/product_(13).jpg",
     name: "Pieza especial, consultar",
   }
 ];
@@ -73,7 +73,7 @@ const product = (data) => {
 showMoreBtn.addEventListener('click',function(){
   container.style.height = !containerStatus ? productListContainer + 90 + "px" : "52px"
   containerStatus = !containerStatus
-  showMoreBtn.innerText = containerStatus ? "- ESCONDER LISTA DE PRODUCTOS" : "+ MOSTRAR LISTA DE PRODUCTOS"
+  showMoreBtn.innerText = containerStatus ? "- Ocultar lista" : "+ Mostrar lista"
 })
 
 const productCarouselGenerator = (data) => {
